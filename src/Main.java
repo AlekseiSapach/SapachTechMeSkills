31fea00b6e5d15754f4a516ba39307691ac6c0ce
Aleksei1364 <sapach1364@gmail.com>
2023-04-25 14:05:29 +0300

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,32 @@
 Начало обучения программирования на языке Java
 Начало обучению Автоматизации тестирования на языке Java
  */
+import java.util.Scanner; //импорт библиотеки?
 public class Main {
     public static void main(String[] args) {
         // Моя первая программа
         System.out.println("Hello Aleksei");
+        System.out.print("Please, enter your name: " + "Сапач Алексей");
+        System.out.println();
+        System.out.println("Please, enter your surname: ");
+        System.out.println("Please, enter your age: ");
+
+        System.out.println("Let's calculate sum of 5 + 10: " + (5 + 10));
+        System.out.println("Алексей" + " " + "Сапач" +
+                ". Welcome to my First " +
+                "Lesson to...........");
+
+        // This is an example comment for Git
+        System.out.println();
+        System.out.print("Please, enter your name & surname: ");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        // New Comment
+
+        String name = "0125";
+        int age = 31;
+        double weight = 88.99;
+        char symbol = 'A';
     }
 }
