c997226fc34344644b7c33f72b29f2f45c8cd086
Aleksei1364 <sapach1364@gmail.com>
2023-04-25 13:58:01 +0300

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+Начало обучения программирования на языке Java
+Начало обучению Автоматизации тестирования на языке Java
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Моя первая программа
+        System.out.println("Hello Aleksei");
+    }
+}
