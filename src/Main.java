0b14d7ad7a87d1368182b767d941899838ac353a
Aleksei1364 <sapach1364@gmail.com>
2023-04-26 21:17:54 +0300

    shape drawing program

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Моя первая программа
-        System.out.println("Hello Aleksei");
+        // Вывод фигуры ёлка
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
     }
 }
