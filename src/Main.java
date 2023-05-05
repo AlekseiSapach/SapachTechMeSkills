e3613be00ee34a0f846e342593c0ecccf6016cdd
Aleksei1364 <sapach1364@gmail.com>
2023-05-05 23:18:36 +0300

    homework 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
-/*
-Начало обучения программирования на языке Java
-Начало обучению Автоматизации тестирования на языке Java
- */
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Моя первая программа
-        System.out.println("Hello Aleksei");
+    public static void main(String[] args)
+    {
+        getPersonData();
+    }
+    public static void getPersonData()
+    {
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Введите Имя: ");
+        String name = enter.nextLine();
+        System.out.print("Введите Фамилию: ");
+        String surname = enter.nextLine();
+        System.out.print("Введите Возраст: ");
+        String age = enter.nextLine();
+        System.out.print("Введите Рост  в см: ");
+        String height = enter.nextLine();
+        System.out.print("Введите Вес в кг: ");
+        String weight = enter.nextLine();
+        System.out.println("Имя и Фамилия: " + name + " " + surname);
+        System.out.println("Возраст: "+ age);
+        System.out.println("Рост "+height + " см" +", Вес: "+weight + " кг");
     }
 }
