8979d88eabf61aec5ce94ab46b172e0401449813
Aleksei1364 <sapach1364@gmail.com>
2023-05-12 22:22:23 +0300

    homework 5 one-dimensional arrays

--- src/DeletingNumber.java
+++ src/DeletingNumber.java
@@ -13,11 +13,23 @@ public class DeletingNumber {
         Scanner enter=new Scanner(System.in);
         int myNumb = enter.nextInt();
         int count =0;
-        int[] myArray= new int[] {6,44,-7,1058,-9685,48,52,109};
+        int[] myArray= new int[] {6,44,-7,105,44,48,52,109};
+        int[] myArray1 = new int[myArray.length];
         for (int index=0; index<myArray.length;index++)
         {
-
+            if (myArray[index]==myNumb)
+            {
+                count++;
+            }else
+                myArray[index-count]=myArray[index];
         }
+        if (count==0)
+            System.out.println("There is no such number in the array");
+        for (int index =0; index<myArray1.length;index++)
+            myArray1[index]=myArray[index];
+        for (int index =0; index<myArray1.length-count;index++)
+            System.out.print(myArray1[index]+" ");
+
 
 
 
