71d5d5bb911055f5412a4e03b133f428b13d1298
Aleksei1364 <sapach1364@gmail.com>
2023-05-12 18:55:36 +0300

    homework 5 one-dimensional arrays

---
+++ src/DeletingNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DeletingNumber {
+    public static void main(String[] args)
+    {
+        getDeleting();
+
+    }
+    public static void getDeleting()
+    {
+        //Удаление числа из массива (задание 1)
+        System.out.print("Enter an integer: ");
+        Scanner enter=new Scanner(System.in);
+        int myNumb = enter.nextInt();
+        int count =0;
+        int[] myArray= new int[] {6,44,-7,1058,-9685,48,52,109};
+        for (int index=0; index<myArray.length;index++)
+        {
+
+        }
+
+
+
+    }
+}
