bc8e982bd0d6a4d3ed6427301b40a448275aca0d
Aleksei1364 <sapach1364@gmail.com>
2023-05-12 18:07:11 +0300

    homework 5 one-dimensional arrays

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-/*
-Начало обучения программирования на языке Java
-Начало обучению Автоматизации тестирования на языке Java
- */
 public class Main {
-    public static void main(String[] args) {
-        // Моя первая программа
-        System.out.println("Hello Aleksei");
+    public static void main(String[] args)
+    {
+
     }
 }
