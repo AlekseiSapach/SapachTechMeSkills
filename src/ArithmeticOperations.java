46a013e5f1e6abc62371fcea465aeb516ec585b3
Aleksei1364 <sapach1364@gmail.com>
2023-05-12 23:48:29 +0300

    homework 5 one-dimensional arrays

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -10,8 +10,29 @@ public class ArithmeticOperations {
         System.out.print("Enter the length of the array: ");
         Scanner enter=new Scanner(System.in);
         int myNumb = enter.nextInt();
-
-
+        int myMax =0;
+        int myMin =0;
+        int mySum=0;
+        int[] myArray2= new int[myNumb];
+        for (int index=0; index<myArray2.length;index++)
+            myArray2[index]=(int) (Math.random()*myNumb);
+        for (int index=0; index<myArray2.length;index++)
+            System.out.print(myArray2[index]+" ");
+        System.out.println();
+        for (int index=0; index<myArray2.length;index++)
+            if(myArray2[index] > myMax)
+                myMax=myArray2[index];
+        System.out.println("Максимальный элемент массива: "+myMax);
+        for (int index=0; index<myArray2.length;index++)
+            myMin=myArray2[0];
+        for (int index=1; index<myArray2.length;index++)
+            if(myMin>myArray2[index])
+                myMin=myArray2[index];
+        System.out.println("Минимальный элемент массива: "+myMin);
+        for (int index=1; index<myArray2.length;index++)
+            mySum+=myArray2[index];
+        double myAverage=mySum/myNumb;
+        System.out.println("Среднее арифметическое всех элементов массива: "+myAverage);
 
 
 
