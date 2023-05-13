4adbf8dc2d7e3014b017301641f205686b574bca
Aleksei1364 <sapach1364@gmail.com>
2023-05-13 14:29:08 +0300

    homework 5 one-dimensional arrays

---
+++ src/AdditionalTask6.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AdditionalTask6 {
+    public static void main(String[] args)
+    {
+        getAddTask2();
+    }
+    public static void getAddTask2()
+    {
+        //Дополнительное задание, создать массив строк и отсортировать его (задание 6)
+
+        String[] myArray= new String[]{"Henry","Scott","Ben","William", "Piers","Fiona","Robert","Tom","Johnny","Adel"};
+
+        for (int index=0; index<myArray.length; index++)
+            System.out.print(myArray[index]+" ");
+        System.out.println();
+        for (int index=0; index<myArray.length; index++)
+            Arrays.sort(myArray);
+        for (int index=0; index<myArray.length; index++)
+            System.out.print(myArray[index]+" ");
+        System.out.println();
+
+
+
+    }
+}
