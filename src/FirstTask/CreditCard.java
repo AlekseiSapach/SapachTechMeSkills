7f855b86317529f977028e93003207025f07af60
Aleksei1364 <sapach1364@gmail.com>
2023-05-19 22:04:34 +0300

    homework 7 classes and objects

--- src/FirstTask/CreditCard.java
+++ src/FirstTask/CreditCard.java
@@ -1,4 +1,28 @@
 package FirstTask;
 
+import java.util.Scanner;
+
 public class CreditCard {
+    double AccountNumber;
+    double CurrentAmount;
+    double Summa;
+
+    void getCardInfo()
+    {
+        System.out.println("Card account number: "+ AccountNumber);
+        System.out.println("Current amount on the card: " + CurrentAmount);
+    }
+    void cashWithdrawal()
+    {
+        Scanner enter = new Scanner(System.in);
+        Summa= enter.nextDouble();
+        CurrentAmount=CurrentAmount-Summa;
+
+    }
+    void getRefill()
+    {
+        Scanner enter = new Scanner(System.in);
+        Summa= enter.nextDouble();
+        CurrentAmount=CurrentAmount+Summa;
+    }
 }
