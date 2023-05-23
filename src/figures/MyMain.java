c643fe42f87c7bb2923c8abcfc0f0bd70e3b0354
Aleksei1364 <sapach1364@gmail.com>
2023-05-23 20:55:32 +0300

    homework 8 OOP

--- src/figures/MyMain.java
+++ src/figures/MyMain.java
@@ -3,8 +3,16 @@ package figures;
 public class MyMain {
     public static void main(String[] args) {
 
-        Triangle triangle = new Triangle(20,10,10,10);
-        triangle.getTriangleArea();
+        FigureOptions[] figureOptions=
+        {
+            new Triangle(5,10,10,10),
+            new Rectangle(10,20),
+            new Circle(5),
+            new Rectangle(15,10),
+            new Triangle(3,5,4,7)
+        };
+
+
 
     }
 }
