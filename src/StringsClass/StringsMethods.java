cc8eb2f7229e0e8e2d2633f829245537ea7f0048
Aleksei1364 <sapach1364@gmail.com>
2023-06-02 20:00:44 +0300

    homework 10 String

---
+++ src/StringsClass/StringsMethods.java
@@ -0,0 +1,36 @@
+package StringsClass;
+
+public class StringsMethods {
+    public static void main(String[] args)
+    {
+
+    }
+    public static void getOutput(String numberDoc)
+    {
+
+    }
+    public static void characterReplacement(String numberDoc)
+    {
+
+    }
+    public static void letterOutput(String numberDoc)
+    {
+
+    }
+    public static void getUppercase(String numberDoc)
+    {
+
+    }
+    public static void entryCheckABC(String numberDoc)
+    {
+
+    }
+    public static void entryCheck555(String numberDoc)
+    {
+
+    }
+    public static void entryCheck1a2b(String numberDoc)
+    {
+
+    }
+}
