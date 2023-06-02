df722ec139b1484f9df84bc35d7ac4649ab66e9d
Aleksei1364 <sapach1364@gmail.com>
2023-06-02 20:37:02 +0300

    homework 10 String

--- src/StringsClass/StringsMethods.java
+++ src/StringsClass/StringsMethods.java
@@ -1,12 +1,36 @@
 package StringsClass;
 
+import java.util.Scanner;
+
 public class StringsMethods {
     public static void main(String[] args)
     {
+        getOutput(input().nextLine());
+        System.out.println();
+        characterReplacement(input().nextLine());
+        System.out.println();
+        letterOutput(input().nextLine());
+        System.out.println();
+        getUppercase(input().nextLine());
+        System.out.println();
+        entryCheckABC(input().nextLine());
+        System.out.println();
+        entryCheck555(input().nextLine());
+        System.out.println();
+        entryCheck1a2b(input().nextLine());
 
+    }
+
+    public static Scanner input()
+    {
+        System.out.print("Введите номер документа формат xxxx-yyy-xxxx-yyy-xyxy, где x — это число, " +
+                "а y — это буква: ");
+        return new Scanner(System.in);
     }
     public static void getOutput(String numberDoc)
     {
+
+
 
     }
     public static void characterReplacement(String numberDoc)
